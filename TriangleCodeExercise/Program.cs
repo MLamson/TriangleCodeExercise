280cc1c33b4aa96b2ba268ee1a8d1b37204a92ed
mlamson <mark_lamson@yahoo.com>
2016-04-28 15:43:15 -0400

    Added PreCoding Notes.

--- TriangleCodeExercise/Program.cs
+++ TriangleCodeExercise/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace TriangleCodeExercise
 {
     class Program
@@ -14,3 +16,4 @@ namespace TriangleCodeExercise
         }
     }
 }
+
