c5e8c76301466ef0b10c1640ff7492b3381368c6
mlamson <mark_lamson@yahoo.com>
2016-04-28 15:33:00 -0400

    Initial Commit

---
+++ TriangleCodeExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleCodeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test of writeline");
+        }
+    }
+}
