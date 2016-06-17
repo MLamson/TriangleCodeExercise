826fc7461cb344ab81a2b45527e356709ab5363f
mlamson <mark_lamson@yahoo.com>
2016-06-17 01:52:33 -0400

    Cleaned up formatting, Decomposed Conditionals, and made variables in tests more descriptive.

--- TriangleCodeExercise/Program.cs
+++ TriangleCodeExercise/Program.cs
@@ -2,38 +2,23 @@
 
 namespace TriangleCodeExercise
 {
-   
-        class Program
+    class Program
     {
-            static void Main(string[] args)
-            {
-                // I did google to find syntax for ToInt32
-                Console.WriteLine("Enter side 1 length");
-                int side1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter side 2 length");
-                int side2 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter side 3 length");
-                int side3 = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            GetInputAndReturnResult();
+        }   
+        internal static string IsTriangle(int triangleSide1, int triangleSide2, int triangleSide3)
+        {
+            string result = "";
 
-                string result = IsTriangle(side1, side2, side3);
-
-                Console.WriteLine(result);
-                Console.ReadLine();
-            }   
-
-
-            internal static string IsTriangle(int triangleSide1, int triangleSide2, int triangleSide3)
-            {
-                string result = "";
-
-            if (triangleSide1 == triangleSide2 && triangleSide2 == triangleSide3)
+            if (AllSidesEqual(triangleSide1, triangleSide2, triangleSide3))
             {
                 result = "Is an equilateral triangle";
             }
-            else if (triangleSide1 + triangleSide2 > triangleSide3 && triangleSide1 + triangleSide3 > triangleSide2 &&
-               triangleSide2 + triangleSide3 > triangleSide1)
+            else if (ValidTriangle(triangleSide1, triangleSide2, triangleSide3))
             {
-                if (triangleSide1 == triangleSide2 || triangleSide1 == triangleSide3 || triangleSide2 == triangleSide3)
+                if (TwoSidesEqual(triangleSide1, triangleSide2, triangleSide3))
                 {
                     result = "Is an isosceles triangle";
                 }
@@ -47,6 +32,37 @@ namespace TriangleCodeExercise
                 result = "Is not a triangle";
             }
             return result;
+        }
+
+        private static bool TwoSidesEqual(int triangleSide1, int triangleSide2, int triangleSide3)
+        {
+            return triangleSide1 == triangleSide2 || triangleSide1 == triangleSide3 || triangleSide2 == triangleSide3;
+        }
+
+        private static bool ValidTriangle(int triangleSide1, int triangleSide2, int triangleSide3)
+        {
+            return triangleSide1 + triangleSide2 > triangleSide3 && triangleSide1 + triangleSide3 > triangleSide2 &&
+                   triangleSide2 + triangleSide3 > triangleSide1;
+        }
+
+        private static bool AllSidesEqual(int triangleSide1, int triangleSide2, int triangleSide3)
+        {
+            return triangleSide1 == triangleSide2 && triangleSide2 == triangleSide3;
+        }
+
+        public static void GetInputAndReturnResult()
+        {
+            Console.WriteLine("Enter side 1 length");
+            int side1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter side 2 length");
+            int side2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter side 3 length");
+            int side3 = Convert.ToInt32(Console.ReadLine());
+
+            string result = IsTriangle(side1, side2, side3);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
--- TriangleCodeExercise/TriangleTest.cs
+++ TriangleCodeExercise/TriangleTest.cs
@@ -13,12 +13,12 @@ namespace TriangleCodeExercise
         [TestCase(5,5,6)]
         [TestCase(5,4,4)]
         [TestCase(7,12,7)]
-        public void Two_Sides_Equal_Test(int a, int b, int c)
+        public void Two_Sides_Equal_Test(int side1, int side2, int side3)
         {
             // Arrange
-            int triangleSide1 = a;
-            int triangleSide2 = b;
-            int triangleSide3 = c;
+            int triangleSide1 = side1;
+            int triangleSide2 = side2;
+            int triangleSide3 = side3;
 
             // Act
             string result = Program.IsTriangle(triangleSide1, triangleSide2, triangleSide3);
@@ -43,12 +43,12 @@ namespace TriangleCodeExercise
       
         [TestCase(2,4,5)]
         [TestCase(8,4,5)]
-        public void Is_Triangle_Passing(int a, int b, int c) //two shorter sides must when added be larger than third side
+        public void Is_Triangle_Passing(int side1, int side2, int side3) //two shorter sides must when added be larger than third side
         {
             // Arrange
-            int triangleSide1 = a;
-            int triangleSide2 = b;
-            int triangleSide3 = c;
+            int triangleSide1 = side1;
+            int triangleSide2 = side2;
+            int triangleSide3 = side3;
 
             // Act
             string result = Program.IsTriangle(triangleSide1, triangleSide2, triangleSide3);
@@ -61,12 +61,12 @@ namespace TriangleCodeExercise
         [TestCase(2,2,4)]
         [TestCase(5,2,2)]
         [TestCase(2,5,2)]
-        public void Is_Triangle_Failing(int a, int b, int c) //two shorter sides must when added be larger than third side
+        public void Is_Triangle_Failing(int side1, int side2, int side3) //two shorter sides must when added be larger than third side
         {
             // Arrange
-            int triangleSide1 = a;
-            int triangleSide2 = b;
-            int triangleSide3 = c;
+            int triangleSide1 = side1;
+            int triangleSide2 = side2;
+            int triangleSide3 = side3;
 
             // Act
             string result = Program.IsTriangle(triangleSide1, triangleSide2, triangleSide3);
